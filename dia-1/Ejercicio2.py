400ee0d21d324a907d852ffe6234799f9447c443
Carlos Manrique Enguita <Manri98@users.noreply.github.com>
2015-07-01 22:09:29 +0200

    Añadidio el primer ejercicio

---
+++ dia-1/Ejercicio2.py
@@ -0,0 +1,7 @@
+i = input("Introduce un número: ")
+while (i%2 == 0):
+    print(i)
+    i = rai = input("Introduce un número: ")
+i = input("Introduce un número: ")
+input("Introduce un número: ")
+print("El número i no es par", i)
